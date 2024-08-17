fb42c862dc9b590a372ddef2f9c1b2f0e7547795
Layrton07 <layrtoncavalcanti09@gmail.com>
2024-08-17 18:14:44 -0300

    modificacao da tela index

--- index.php
+++ index.php
@@ -12,22 +12,35 @@
         <h1>history linked list</h1>
         <div class="container-box-game">
             <div class="container-btn" id="container-btn">
+                <audio id="buttonSound" src="./sound/click.mp3" style="display: none;"></audio>
                 <button class="btn-play" onclick="play()"></button>
                 <button class="btn-rank" onclick="rank()"></button>
-                <!-- <button>tessssteeeee</button> -->
             </div>
-            <div class="container-play" id="container-play">
-                <div class="inline-box">
-                    <button class="btn-goBack" onclick="goBack()"></button>
-                    <h2>choose difficulty</h2>
-                </div>
-                <button class="easy-btn" onclick="easyBtn()"></button>
-                <button class="medium-btn" onclick="mediumBtm()"></button>
-                <button class="hard-btn" onclick="hardBtn()"></button>
+            <div class="container-name-skin" id="container-name-skin">
+                <form action="game-levels/levels.php" method="POST">
+                    <div class="form">
+                        <button type="button" class="btn-goBack" onclick="goBack()"></button>
+                        <h2>Enter your name:</h2>
+                        <input type="text" name="name"required>
+                    </div>
+                    <br>
+                    <div class="characters">
+                        <input type="radio" name="skin" class="skin-1" value="Milta_teste.png" required>
+                        <input type="radio" name="skin" class="skin-2" value="Milta_Blue_teste.png" required>
+                    </div>
+                    <br>
+                    <div class="container-btn-submit-form">
+                        <input type="submit" value="" class="btn-play-2">
+                    </div>
+                </form>
             </div>
             <div class="container-rank" id="container-rank">
                 <button class="btn-goBack-2" id="btn-goBack-2" onclick="goBack2()"></button>
-                <div class="rank"></div>
+                <div class="rank">
+                    <div class="text-rank">
+                        
+                    </div>
+                </div>
             </div>
             <div class="container-box-game-inner">
             </div>
