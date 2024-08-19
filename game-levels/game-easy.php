3c1b5139a474a7b73a056e675c7dd0359aed4d1d
Layrton07 <layrtoncavalcanti09@gmail.com>
2024-08-19 20:17:28 -0300

    fazendo o front

--- game-levels/game-easy.php
+++ game-levels/game-easy.php
@@ -8,7 +8,7 @@ $nivel = $_POST['nivel'];
 
 require_once '../Poo/Classes/Game.php';
 
-$game = new Game($name, $skin);
+$game = new Game($name, $skin,$nivel);
 
 
 ?>
@@ -34,6 +34,15 @@ $game = new Game($name, $skin);
                 <div class="cloud cloud-2">
                 </div>
                 <div class="container-characters">
+                    <div class="container-life">
+                        <?php
+
+                            $life = $game->getAvatar()->getVida();
+                            for ($i=1;$i<=$life;$i++){
+                                echo '<image id="life-'.$i.'" src="../Imagens/Coracao/1_coracao_cheio_cut.png"/>';
+                            }
+                        ?>
+                    </div>
                     <?php
                         //alterar depois
                         //echo ($game->getAvatar()->getNome());
@@ -44,20 +53,42 @@ $game = new Game($name, $skin);
                         // echo "</div>";
 
                         echo $game->getAvatar()->getVida();
-                        $a = $game->getCaminho()->exibirBlocos();
+                        // $a = $game->getCaminho()->exibirBlocos();
 
-                        for ($i = 0; $i < count($a); $i++) { 
-                            echo $a[$i]->getTipo();
-                            echo $a[$i]->getSkin();
+                        // for ($i = 0; $i < count($a); $i++) { 
+                        //     echo $a[$i]->getTipo();
+                        //     echo $a[$i]->getSkin();
                             
-                        }
-
-                        
-
-                        
+                        // }
 
                     ?>
-                    <div class="floor"></div>
+                    <div class="box-character">
+                        <?php
+                            echo "<div class='character' style=\"background-image:url('../Imagens/Personagem/" . $game->getAvatar()->getSkin() . "');\"></div>";
+                            // echo "<div class='character' style='background-image: url('../Imagens/Personagem/".$game->getAvatar()->getSkin().");'></div>";
+                            // echo "../imagens/personagem/".$game->getAvatar()->getSkin();
+                        ?>
+                        <div class="container-btn-game">
+                            <button class="jump" on></button>
+                            <button class="jump"></button>
+                            <button class="jump"></button>
+                        </div>
+                    </div>
+                    <div class="floor">
+                        <?php
+                            $a = $game->getCaminho()->exibirBlocos();
+
+                            for ($i = 0; $i < count($a); $i++) { 
+                                echo $a[$i]->getTipo();
+                                echo $a[$i]->getSkin()." ";
+                                
+                            }
+
+                            // for ($i = 0; $i < count($a); $i++) { 
+                            //     echo '<div class="bloco" style="width: 200px; height: 200px; background-image: url("../Imagens/Plataforma/");"></div>';
+                            // }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
